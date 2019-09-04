a77c8caa59e3986f2cd4c3776e435868b48464f6
Taliaa <taliaa10@ymail.com>
2019-09-04 00:19:22 -0500

    port

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 1000;
-const path = require('path');
-const VIEWS_PATH = path.join(__dirname, '/views');
-const fs = require('fs');
+const PORT = process.env.PORT || 8000;
+const path = require("path");
+const VIEWS_PATH = path.join(__dirname, "/views");
+const fs = require("fs");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, `utf-8`);
 const portfolioData = JSON.parse(json);
 
-app.set('views', VIEWS_PATH);
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("views", VIEWS_PATH);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.locals.portfolioData = portfolioData;
-  res.render('index');
+  res.render("index");
 });
 
 app.listen(PORT, () => {

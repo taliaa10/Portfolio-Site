ce26a76ecf9e5f2b57795f4c67c9876abc418148
Taliaa <taliaa10@ymail.com>
2019-09-21 18:48:37 -0500

    resume

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const path = require("path");
 const VIEWS_PATH = path.join(__dirname, "/views");
 const fs = require("fs");
